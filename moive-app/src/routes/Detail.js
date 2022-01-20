7ee48a1ada1eb244d755da3ec2ef3e262f4de824
wldnd9904 <wldnd9904@uos.ac.kr>
2022-01-21 00:35:55 +0900

    route 2

--- moive-app/src/routes/Detail.js
+++ moive-app/src/routes/Detail.js
@@ -1,4 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 function Detail() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [detail, setDetail] = useState({});
+
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return <h1>Detail</h1>;
 }
 
