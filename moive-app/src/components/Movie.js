0780aae72ff23065a8e13db003321873f675ff5b
wldnd9904 <wldnd9904@uos.ac.kr>
2022-01-21 14:27:41 +0900

    a bit css

--- moive-app/src/components/Movie.js
+++ moive-app/src/components/Movie.js
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import styles from "./Movie.module.css";
 
-function Movie({ id, movie }) {
+function Movie({ movie }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const onload = () => {
+    setIsLoading(false);
+  };
   return (
-    <div key={movie.id}>
-      <img src={movie.medium_cover_image} alt="img" />
-      <h2>
-        <nav>
-          <Link to={`/movie/${id}`}>{movie.title}</Link>
-        </nav>
-      </h2>
-      <p>{movie.summary}</p>
-      {movie.hasOwnProperty("genres") ? (
-        <ul>
-          {movie.genres.map((g) => (
-            <li key={g}>{g}</li>
-          ))}
-        </ul>
-      ) : null}
+    <div
+      className={styles.movieBox}
+      key={movie.id}
+      loaded={isLoading ? "false" : "true"}
+    >
+      <nav>
+        <Link to={`/movie/${movie.id}`}>
+          <img
+            src={movie.medium_cover_image}
+            onLoad={onload}
+            alt={movie.title}
+          />
+        </Link>
+      </nav>
     </div>
   );
 }
--- moive-app/src/routes/Home.js
+++ moive-app/src/routes/Home.js
@@ -1,32 +1,10 @@
-import { useState, useEffect } from "react";
-import Movie from "../components/Movie";
+import MovieList from "../components/MovieList";
 
 function Home() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
   return (
     <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie key={movie.id} id={movie.id} movie={movie} />
-          ))}
-        </div>
-      )}
+      <MovieList />
+      <MovieList />
     </div>
   );
 }
